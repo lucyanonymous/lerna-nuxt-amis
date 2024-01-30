070ff802fa40fb0eabdb2e150d23b8dcc288e4db
lucyluxiliu <lucyluxiliu@tencent.com>
2024-01-30 11:20:44 +0800

    add npm use

---
+++ packages/project/components/AMISRenderer2.vue
@@ -0,0 +1,13 @@
+<script>
+import { render as renderSchema } from 'amis';
+export default {
+  name: 'AMISRenderer2',
+
+  render() {
+    return renderSchema({
+      type: 'page',
+      body: 'Hello World!',
+    });
+  },
+};
+</script>
---
+++ packages/project/pages/Form.vue
@@ -0,0 +1,15 @@
+<template>
+  <client-only>
+    <AMISRenderer2 />
+  </client-only>
+</template>
+
+<script>
+import AMISRenderer2 from '../components/AMISRenderer2';
+export default {
+  name: 'Form',
+  components: {
+    AMISRenderer2,
+  },
+};
+</script>
